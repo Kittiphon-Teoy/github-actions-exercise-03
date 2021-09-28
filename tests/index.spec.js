2d12855f8734f0d213c80d9c3b69ef3732b5ad8a
Kittiphon <61160086@go.buu.ac.th>
2021-09-28 20:18:56 +0700

    TestV2

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('GET /', () => {
-    it('responds with "No user in the system"', (done) => {
-        request(app).get('/user').expect('No user in the system', done)
+    it('responds with "Hello world!"', (done) => {
+        request(app).get('/').expect('Hello world!', done)
     })
 })
