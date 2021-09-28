98d94acb7c8752ca14dd5e0f4fc92d48e46a5649
Kittiphon <61160086@go.buu.ac.th>
2021-09-28 20:29:30 +0700

    Testv3

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('GET /', () => {
-    it(res.json({message: 'Hello world!'}), (done) => {
-        request(app).get('/').expect(res.json({message: 'Hello world!'}), done)
+    it('{"message":"Hello world!"}', (done) => {
+        request(app).get('/').expect('{"message":"Hello world!"}', done)
     })
 })
